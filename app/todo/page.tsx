2e1825ff24ecf6feae912de801b67a44e4901512
illahibux <illahibuxjagirani@gmail.com>
2024-05-07 00:23:59 +0500

    index

--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -1,9 +1,9 @@
-'use client'
-import React, { useState } from 'react'
+"use client";
+import React, { useState } from "react";
 
-interface Task{
-  task: string,
-  detail: string
+interface Task {
+  task: string;
+  detail: string;
 }
 
 function ToDo() {
@@ -11,33 +11,40 @@ function ToDo() {
   let [detail, setDetail] = useState("");
   let [mainTask, setMainTask] = useState<Task[]>([]);
 
-
   // Add Task Button Working
-    function taskHandler(e: any){
+  function taskHandler(e: any) {
     e.preventDefault();
     setTask("");
-    setDetail("")
-    setMainTask([...mainTask, {task, detail}])
-    }
+    setDetail("");
+    setMainTask([...mainTask, { task, detail }]);
+  }
 
-  let savedTasks: any = <div className='
+  let savedTasks: any = (
+    <div
+      className="
   font-bold
   text-center
   pt-10
   text-6xl
-  '>No Task Available</div>
+  "
+    >
+      No Task Available
+    </div>
+  );
 
   // Delete Button
-  function deleteHandler(e:any){
+  function deleteHandler(e: any) {
     let copyMainTask = [...mainTask];
     copyMainTask.splice(e, 1);
     setMainTask(copyMainTask);
   }
 
-  if(mainTask.length > 0){
-    savedTasks = mainTask.map((e)=>{
-
-      return <div className='
+  if (mainTask.length > 0) {
+    savedTasks = mainTask.map((e, index) => {
+      return (
+        <div
+          key={index}
+          className="
       bg-gradient-to-br from-purple-500 to-green-500
       flex
       justify-around
@@ -47,31 +54,37 @@ function ToDo() {
       mb-1
       lg:flex-row
       gap-4
-      '>
-        <h1 className='lg:w-1/4 text-wrap text-start font-bold'>{e.task}</h1>
+      "
+        >
+          <h1 className="lg:w-1/4 text-wrap text-start font-bold">{e.task}</h1>
 
-        <h1 className='lg:w-1/2 text-start text-wrap'>{e.detail}</h1>
+          <h1 className="lg:w-1/2 text-start text-wrap">{e.detail}</h1>
 
-        <button onClick={deleteHandler}
-        className='bg-red-500 lg:w-1/4 hover:bg-red-600
+          <button
+            onClick={deleteHandler}
+            className="bg-red-500 lg:w-1/4 hover:bg-red-600
         text-white
         p-1
-        '
-        >Delete</button>
-      </div>
-
-
-    })
+        "
+          >
+            Delete
+          </button>
+        </div>
+      );
+    });
   }
 
   return (
-    <div className='
+    <div
+      className="
     mainBg
     min-h-[calc(100vh-90px)]
     px-4 py-1
-    '>
-      <div className='w-full h-full flex flex-col'>
-      <h1 className='
+    "
+    >
+      <div className="w-full h-full flex flex-col">
+        <h1
+          className="
       bg-black
       lg:text-3xl
       text-white
@@ -79,13 +92,15 @@ function ToDo() {
       text-center
       text-sm
       p-1
-      '>My ToDo List</h1>
+      "
+        >
+          My ToDo List
+        </h1>
 
-
-      {/* input task */}
-      <form className='flex flex-col lg:flex-row gap-2 py-4'>
-
-        <input className='
+        {/* input task */}
+        <form className="flex flex-col lg:flex-row gap-2 py-4">
+          <input
+            className="
         text-sm
         lg:text-2xl
         border-blue-800
@@ -93,14 +108,17 @@ function ToDo() {
         outline-none
         rounded
         p-1
-        '
-        type="text" 
-        placeholder='Enter Task Here'
-        value={task}
-        onChange={(e)=>{setTask(e.target.value)}}
-        />
+        "
+            type="text"
+            placeholder="Enter Task Here"
+            value={task}
+            onChange={(e) => {
+              setTask(e.target.value);
+            }}
+          />
 
-      <input className='
+          <input
+            className="
       text-sm
       lg:text-2xl
       border-blue-800
@@ -108,14 +126,18 @@ function ToDo() {
       outline-none
       rounded
       p-1
-      ' type="text" 
-      placeholder='Enter Details Here'
-      value={detail}
-      onChange={(e)=>{setDetail(e.target.value)}}
-       />
+      "
+            type="text"
+            placeholder="Enter Details Here"
+            value={detail}
+            onChange={(e) => {
+              setDetail(e.target.value);
+            }}
+          />
 
-      <button onClick={taskHandler}
-      className='
+          <button
+            onClick={taskHandler}
+            className="
       bg-blue-800
       text-white
       p-1
@@ -124,17 +146,17 @@ function ToDo() {
       font-bold
       rounded
       hover:bg-blue-900
-      '>Add Task</button>
-      </form>
+      "
+          >
+            Add Task
+          </button>
+        </form>
 
-    {/* Saved Tasks */}
-      <div>
-      {savedTasks}
-      </div>
-
+        {/* Saved Tasks */}
+        <div>{savedTasks}</div>
       </div>
     </div>
-  )
+  );
 }
 
-export default ToDo
+export default ToDo;
